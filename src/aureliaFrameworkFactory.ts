585c0a1a97d43767212172f8914521b7324b3b3e
swalters <swalters@singletreetech.com>
2017-01-13 11:10:34 -0600

    Add support for fullWidthCellRenderer and components
    declarative:
               <ag-full-width-row-template>
                  <span style="font-weight: bold">${params.data.name}</span>
                </ag-full-width-row-template>
    
     imperative:
      gridOptions.fullWidthCellRendererFramework = {template:NameAndAge};
    
     gridOptions.fullWidthCellRendererFramework =  {template:`<template><name-and-age></name-and-age></template>`};
    
    Add support for Components.  To use:
       gridOptions.fullWidthCellRendererFramework = {component:NameAndAge};
    
     Any of the Framework properties can have either .template or .component populated

--- src/aureliaFrameworkFactory.ts
+++ src/aureliaFrameworkFactory.ts
@@ -1,4 +1,8 @@
-import {autoinject, transient, Container, ViewResources, ViewCompiler} from "aurelia-framework";
+import {
+    autoinject, transient, Container, ViewResources, ViewCompiler,
+    ViewSlot, DOM, Origin, TemplatingEngine,
+    ViewFactory
+} from "aurelia-framework";
 
 import {
     ICellRenderer,
@@ -13,6 +17,12 @@ import {
 
 import {AureliaComponentFactory} from "./aureliaComponentFactory";
 
+export interface IAureliaRendererFramework {
+    component?: string|Function;
+    template?: string;
+    $viewFactory?: ViewFactory;
+}
+
 @autoinject()
 @transient()
 export class AureliaFrameworkFactory implements IFrameworkFactory {
@@ -20,39 +30,62 @@ export class AureliaFrameworkFactory implements IFrameworkFactory {
     private _viewResources: ViewResources;
     private _baseFrameworkFactory: IFrameworkFactory = new BaseFrameworkFactory();    // todo - inject this
 
-    constructor(private _componentFactory: AureliaComponentFactory, private _viewCompiler: ViewCompiler) {
+    constructor(private _componentFactory: AureliaComponentFactory,
+                private _viewCompiler: ViewCompiler,
+                private templatingEngine: TemplatingEngine) {
     }
 
     public colDefFloatingCellRenderer(colDef: ColDef): {new(): ICellRenderer} | ICellRendererFunc | string {
         return this._baseFrameworkFactory.colDefFloatingCellRenderer(colDef);
     }
 
+    public getCellRendererViewFactory(colDef: ColDef): Promise<void> {
+        return this.populateViewFactory(colDef.cellEditorFramework);
+    }
     public colDefCellRenderer(colDef: ColDef): {new(): ICellRenderer} | ICellRendererFunc | string {
-        if (colDef.cellRendererFramework) {
-            if (!colDef.cellRendererFramework.$viewFactory) {
-                colDef.cellRendererFramework.$viewFactory = this._viewCompiler.compile(colDef.cellRendererFramework.template, this._viewResources);
-            }
-
-            return this._componentFactory.createRendererFromTemplate(this._container, colDef.cellRendererFramework.$viewFactory);
-        } else {
+        let rendererFramework = colDef.cellRendererFramework;
+        //$viewFactory must be created before the grid is initialized
+        //because promises are involved.
+        if (rendererFramework && (rendererFramework.$viewFactory)) {
+            return this._componentFactory.createCellRendererFromViewFactory(this._container,
+                                                                            rendererFramework.$viewFactory);
+        }
+        else {
             return this._baseFrameworkFactory.colDefCellRenderer(colDef);
         }
     }
 
+    public getCellEditorViewFactory(colDef: ColDef): Promise<void> {
+        return this.populateViewFactory(colDef.cellEditorFramework);
+    }
     public colDefCellEditor(colDef: ColDef): {new(): ICellEditor} | string {
-        if (colDef.cellEditorFramework) {
-            //cache the columnDef viewFactory
-            if (!colDef.cellEditorFramework.$viewFactory) {
-                colDef.cellEditorFramework.$viewFactory = this._viewCompiler.compile(colDef.cellEditorFramework.template, this._viewResources);
-            }
-            return this._componentFactory.createEditorFromTemplate(this._container, colDef.cellEditorFramework.$viewFactory);
-
-        } else {
+        let rendererFramework = colDef.cellEditorFramework;
+        //$viewFactory must be created before the grid is initialized
+        //because promises are involved.
+        if (rendererFramework && (rendererFramework.$viewFactory)) {
+            return this._componentFactory.createEditorFromTemplate(this._container,
+                                                                   rendererFramework.$viewFactory);
+        }
+        else {
             return this._baseFrameworkFactory.colDefCellEditor(colDef);
         }
     }
 
+    public getFullWidthCellRendererViewFactory(gridOptions: GridOptions): Promise<void> {
+        return this.populateViewFactory(gridOptions.fullWidthCellRendererFramework);
+    }
+
     public gridOptionsFullWidthCellRenderer(gridOptions: GridOptions): {new(): ICellRenderer} | ICellRendererFunc | string {
+        let rendererFramework = gridOptions.fullWidthCellRendererFramework;
+
+        //$viewFactory must be created before the grid is initialized
+        //because promises are involved.
+        if (rendererFramework && (rendererFramework.$viewFactory)) {
+            return this._componentFactory
+                       .createCellRendererFromViewFactory(this._container, rendererFramework.$viewFactory);
+
+        }
+
         return this._baseFrameworkFactory.gridOptionsFullWidthCellRenderer(gridOptions);
     }
 
@@ -76,7 +109,59 @@ export class AureliaFrameworkFactory implements IFrameworkFactory {
         this._viewResources = viewResources;
     }
 
-    setTimeout(action: any, timeout?: any): void {
-        this._baseFrameworkFactory.setTimeout(action, timeout);
+    setTimeout(action: any, timeout?: any): number {
+        return this._baseFrameworkFactory.setTimeout(action, timeout);
     }
+
+    /**
+     * Aurelia returns a promise when creating a viewFactory for a component
+     */
+    private populateViewFactory(rendererFramework: IAureliaRendererFramework): Promise<void> {
+        if (!rendererFramework || (!rendererFramework.template && !rendererFramework.component)) {
+            return Promise.resolve();
+        }
+
+        if(rendererFramework.template && rendererFramework.component){
+            console.warn('Both template and component are present for the Aurelia renderer framework. Using Template');
+        }
+
+        if (rendererFramework.template) {
+            rendererFramework.$viewFactory = this._viewCompiler.compile(rendererFramework.template,
+                                                                        this._viewResources);
+            return Promise.resolve();
+        }
+
+        if (rendererFramework.component) {
+            return this.createViewFactoryFromComponent(rendererFramework.component)
+                .then(ViewFactory => {rendererFramework.$viewFactory = ViewFactory});
+        }
+
+    }
+
+    private createViewFactoryFromComponent(component: string | Function): Promise<ViewFactory> {
+        let host = DOM.createElement('div');
+
+        let viewModel = typeof component === 'function'
+            ? Origin.get(component).moduleId
+            : component;
+
+        let viewSlot = new ViewSlot(host, true);
+
+        let compositionContext = {
+            container: this._container,
+            viewModel: viewModel,
+            viewSlot: viewSlot,
+            viewResources: this._viewResources,
+            model: {}
+        };
+
+        return this.templatingEngine.compose(<any>compositionContext)
+                   .then((controller: any) => {
+                       //throw away the view we just compiled
+                       //we only want the view factory
+                       viewSlot.removeAll(true, true);
+                       return controller.view.viewFactory;
+                   });
+    }
+
 }
